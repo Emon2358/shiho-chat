9bf200d82f190074a1538bcb97e10043025e2aff
kjgd <144606308+Emon2358@users.noreply.github.com>
2025-07-27 23:13:51 +0900

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: このファイルは Next.js が自動生成・管理します。
+// 削除しないでください。
